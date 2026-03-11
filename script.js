54588ef0c8bec770047588d9bc327e46dc0699cd
tarunyoutub67-stack <tarun.youtub67@gmail.com>
2026-03-11 15:03:46 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,14 @@
-function calculateLoan(){
+function calculateEMI(){
 
-let principal = parseFloat(document.getElementById("principal").value);
-let annualRate = parseFloat(document.getElementById("loanRate").value);
-let years = parseFloat(document.getElementById("loanYears").value);
+let P = document.getElementById("loan").value;
+let annualRate = document.getElementById("rate").value;
+let n = document.getElementById("months").value;
 
-let monthlyRate = annualRate / 12 / 100;
-let months = years * 12;
+let r = annualRate / 12 / 100;
 
-let emi = (principal * monthlyRate * Math.pow(1 + monthlyRate, months)) /
-          (Math.pow(1 + monthlyRate, months) - 1);
+let emi = (P * r * Math.pow(1+r,n)) / (Math.pow(1+r,n)-1);
 
-let totalPayment = emi * months;
-let interest = totalPayment - principal;
-
-document.getElementById("loanResult").innerHTML =
-"Monthly EMI: ₹ " + emi.toFixed(2) +
-"<br>Total Interest: ₹ " + interest.toFixed(2) +
-"<br>Total Payment: ₹ " + totalPayment.toFixed(2);
+document.getElementById("result").innerHTML = 
+"Monthly EMI: ₹ " + emi.toFixed(2);
 
 }
