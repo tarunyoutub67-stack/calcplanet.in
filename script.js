f91f63783bea7c65263964ab5ccfee146e124930
tarunyoutub67-stack <tarun.youtub67@gmail.com>
2026-03-11 15:42:47 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,43 @@
+function calculateEMI(){
+
+let P = parseFloat(document.getElementById("loan").value);
+let rate = parseFloat(document.getElementById("rate").value);
+let months = parseFloat(document.getElementById("months").value);
+
+if(!P || !rate || !months){
+document.getElementById("emiResult").innerHTML="Enter all values";
+return;
+}
+
+let r = rate/12/100;
+
+let emi = (P*r*Math.pow(1+r,months))/(Math.pow(1+r,months)-1);
+
+document.getElementById("emiResult").innerHTML =
+"Monthly EMI: ₹ "+emi.toFixed(2);
+
+}
+
+
+
 function calculateLoan(){
 
-let P = parseFloat(document.getElementById("amount").value);
+let loan = parseFloat(document.getElementById("loan").value);
 let rate = parseFloat(document.getElementById("rate").value);
 let years = parseFloat(document.getElementById("years").value);
+
+if(!loan || !rate || !years){
+document.getElementById("loanResult").innerHTML="Enter all values";
+return;
+}
 
 let r = rate/12/100;
 let n = years*12;
 
-let emi = (P*r*Math.pow(1+r,n))/(Math.pow(1+r,n)-1);
+let emi = (loan*r*Math.pow(1+r,n))/(Math.pow(1+r,n)-1);
 
 let total = emi*n;
-let interest = total-P;
+let interest = total-loan;
 
 document.getElementById("loanResult").innerHTML =
 "EMI: ₹ "+emi.toFixed(2)+"<br>Total Interest: ₹ "+interest.toFixed(2);
