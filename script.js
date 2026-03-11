72006d3f28ff4490d95bee13ed58031f110504e0
tarunyoutub67-stack <tarun.youtub67@gmail.com>
2026-03-11 15:36:29 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,79 +1,18 @@
-function calculateEMI(){
-
-let P = parseFloat(document.getElementById("loan").value);
-let rate = parseFloat(document.getElementById("rate").value);
-let months = parseFloat(document.getElementById("months").value);
-
-let r = rate/12/100;
-
-let emi = (P*r*Math.pow(1+r,months))/(Math.pow(1+r,months)-1);
-
-document.getElementById("emiResult").innerHTML =
-"Monthly EMI: ₹ "+emi.toFixed(2);
-
-}
-
-function calculateSIP(){
-
-let sip = parseFloat(document.getElementById("sip").value);
-let rate = parseFloat(document.getElementById("rate").value)/12/100;
-let months = parseFloat(document.getElementById("months").value);
-
-let future = sip*((Math.pow(1+rate,months)-1)/rate)*(1+rate);
-
-document.getElementById("sipResult").innerHTML =
-"Future Value: ₹ "+future.toFixed(2);
-
-}
-
-function calculateFD(){
-
-let amount = parseFloat(document.getElementById("amount").value);
-let rate = parseFloat(document.getElementById("rate").value)/100;
-let years = parseFloat(document.getElementById("years").value);
-
-let maturity = amount*Math.pow((1+rate),years);
-
-document.getElementById("fdResult").innerHTML =
-"Maturity Amount: ₹ "+maturity.toFixed(2);
-
-}
-
 function calculateLoan(){
 
-let principal = parseFloat(document.getElementById("amount").value);
-let rate = parseFloat(document.getElementById("rate").value)/100;
+let P = parseFloat(document.getElementById("amount").value);
+let rate = parseFloat(document.getElementById("rate").value);
 let years = parseFloat(document.getElementById("years").value);
 
-let interest = principal*rate*years;
+let r = rate/12/100;
+let n = years*12;
+
+let emi = (P*r*Math.pow(1+r,n))/(Math.pow(1+r,n)-1);
+
+let total = emi*n;
+let interest = total-P;
 
 document.getElementById("loanResult").innerHTML =
-"Total Interest: ₹ "+interest.toFixed(2);
-
-}
-
-function calculateCAGR(){
-
-let start = parseFloat(document.getElementById("start").value);
-let end = parseFloat(document.getElementById("end").value);
-let years = parseFloat(document.getElementById("years").value);
-
-let cagr = (Math.pow(end/start,1/years)-1)*100;
-
-document.getElementById("cagrResult").innerHTML =
-"CAGR: "+cagr.toFixed(2)+" %";
-
-}
-
-function calculateCompound(){
-
-let amount = parseFloat(document.getElementById("amount").value);
-let rate = parseFloat(document.getElementById("rate").value)/100;
-let years = parseFloat(document.getElementById("years").value);
-
-let total = amount*Math.pow((1+rate),years);
-
-document.getElementById("compoundResult").innerHTML =
-"Total Amount: ₹ "+total.toFixed(2);
+"EMI: ₹ "+emi.toFixed(2)+"<br>Total Interest: ₹ "+interest.toFixed(2);
 
 }
